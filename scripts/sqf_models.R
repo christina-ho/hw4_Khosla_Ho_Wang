c91fcf332ea6f45fc80435f9188937b139ea09c6
dk3606 <dk3606@nyu.edu>
2019-10-22 16:41:46 -0400

    added b2 start

--- scripts/sqf_models.R
+++ scripts/sqf_models.R
@@ -1,5 +1,6 @@
 require(tidyverse)
-
+require(gtools)
+require(ROCR)
 
 # B1
 
@@ -22,6 +23,29 @@ train_sqf <- sqf.data[1:(p20*3),]
 validation_sqf <- sqf.data[((p20*3)+1):(p20*4),]
 test_sqf <- sqf.data[((p20*4)+1):nrow(sqf.data),]
 
+# B2
+
+model_performance <- tibble(feature_one = rep(NA,528),feature_two = rep(NA,528),validation_auc = rep(NA,528))
+
+names <- colnames(sqf.data)
+names <- names[-c(1,2,3,4)]
+
+test <- combinations(33,2,names)
+
+model_performance$feature_one <- test[,1]
+model_performance$feature_two <- test[,2]
+
+for(i in 1:nrow(model_performance)){
+  traindata <- train_sqf %>% 
+    select(found.weapon,as.character(model_performance[i,1]),as.character(model_performance[i,2]),precinct)
+  valdata <- validation_sqf %>% 
+    select(found.weapon,as.character(model_performance[i,1]),as.character(model_performance[i,2]),precinct)
+  trainfit <- glm(found.weapon ~ .,family = binomial,data = traindata)
+  predictions <- predict(trainfit,valdata,type = 'response')
+  rocr.pred <- prediction(predictions, valdata$found.weapon)
+  model_performance[i,3] <- performance(rocr.pred, "auc")@y.values[[1]]
+  print(i)
+}
 
 
 
