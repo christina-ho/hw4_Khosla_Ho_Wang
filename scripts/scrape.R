a82ba3907247f0b444c871e042d34dd6a4b4abcf
dk3606 <dk3606@nyu.edu>
2019-10-24 16:18:09 -0400

    updated A

--- scripts/scrape.R
+++ scripts/scrape.R
@@ -11,6 +11,32 @@ neighbourhood <- gsub(" ","-",neighbourhood)
 
 Nlinks <- paste0('https://www.universalhub.com/crime/',tolower(neighbourhood),'.html')
 
+type_extractor <- function(links){
+  content <- read_html(links)
+  type <- html_nodes(x=content, xpath='//td[contains(@class,"name")]')
+  type <- html_text(type,trim = T)
+  type
+}
+
+type_extractor(Nlinks[1])
+
+time_extractor <- function(links){
+  content <- read_html(links)
+  time <- html_nodes(x=content, xpath='//td[contains(@class,"date")]')
+  time <- html_text(time,trim = T)
+  time
+}
+
+time_extractor(Nlinks[1])
+
+crime_data <- tibble('neighborhood' = neighbourhood)
+
+crime_data <- neighbourhood %>% rowwise()
+
+
+
+
+
 
 
 
