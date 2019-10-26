4f824e5a6451ec88fc34c898027a0f9cd5989d3f
dk3606 <dk3606@nyu.edu>
2019-10-26 12:52:33 -0400

    table created

--- scripts/scrape.R
+++ scripts/scrape.R
@@ -10,6 +10,10 @@ neighbourhood <- html_text(neighbourhood,trim = T)
 neighbourhood <- gsub(" ","-",neighbourhood)
 
 Nlinks <- paste0('https://www.universalhub.com/crime/',tolower(neighbourhood),'.html')
+Nlinks <- c(Nlinks,'https://www.universalhub.com/crime/dorchester.html?page=1','https://www.universalhub.com/crime/mission-hill','https://www.universalhub.com/newcrime/9967')
+Nlinks <- Nlinks[-c(6,14)]
+
+
 
 type_extractor <- function(links){
   content <- read_html(links)
@@ -30,14 +34,28 @@ time_extractor <- function(links){
 
 time_extractor(Nlinks[1])
 
-Ns <- tibble('neighborhood' = neighbourhood, 'link' = Nlinks)
+lengths <- NA
 
-crime_data <- Ns[1,] %>% rowwise() %>% do(crime = type_extractor(.$link))
-hou
+for(i in 1:length(Nlinks)){
+  lengths[i] <- length(time_extractor(Nlinks[i]))
+}
+ 
+nbhood <- c(neighbourhood[-c(6,14)],"Dorchester","Mission-Hill","Chinatown")
 
+neighborhoods <- vector()
+types1 <- vector()
+types <- vector()
+time1 <- vector()
+time <- vector()
 
+for(i in 1:21){
+  neighbourhood1 <- rep(nbhood[i],lengths[i])
+  neighborhoods <- c(neighborhoods,neighbourhood1)
+  types1 <- type_extractor(Nlinks[i])
+  types <- c(types,types1)
+  time1 <- time_extractor(Nlinks[i])
+  time <- c(time,time1)
+}
 
-
-
-
+crime_data <- tibble('crime' = types, 'hour' = time, 'neighborhood' = neighborhoods)
 
